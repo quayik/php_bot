c3e6ceb354fc1e243b03353ae00f8d557c2472ee
shaded92 <damir.shaltaev@gmail.com>
2021-03-24 19:21:21 +0600

    reply in current chat and formatted code

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $update = json_decode(file_get_contents("php://input"), TRUE);
 $chatId = $update['message']['chat']['id'];
 $iMessageText = $update['message']['text'];
 $url = get_url($chatId, "test")
-$temp = file_get_contents();
+$temp = file_get_contents($url);
 
 function get_url($chatId, $text){
     $endpoint = 'https://api.telegram.org/bot';
