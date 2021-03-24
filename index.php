d9fba746663efe9de5f79aa75dd38dfab3d5b634
shaded92 <damir.shaltaev@gmail.com>
2021-03-24 19:43:11 +0600

    reply in current chat and formatted code try6

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $testUrl = 'https://api.telegram.org/bot1705693811:AAFRF0B5Z2BNaRC9kIDY1p_gMOI_ZNPz8OM/sendMessage?chat_id=548527630&text=1';
 $temp1 = file_get_contents($testUrl);
-$update = json_decode(file_get_contents("php://input"), TRUE);
+/*$update = json_decode(file_get_contents("php://input"), TRUE);
 $chatId = $update['message']['chat']['id'];
 $iMessageText = $update['message']['text'];
 $temp2 = file_get_contents('https://api.telegram.org/bot1705693811:AAFRF0B5Z2BNaRC9kIDY1p_gMOI_ZNPz8OM/sendMessage?chat_id=548527630&text=2');
@@ -14,4 +14,4 @@ function get_url($chatId, $text){
     $botToken = '1705693811:AAFRF0B5Z2BNaRC9kIDY1p_gMOI_ZNPz8OM';
     $url = $endpoint . $botToken . '/sendMessage?chat_id=' . $chatId . '&text=' . $text;
     return $url;
-}
+}*/
