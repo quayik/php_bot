8705efb3cee25fe892978470d5f3425598223e2a
shaded92 <damir.shaltaev@gmail.com>
2021-03-24 19:05:48 +0600

    get content of post

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 $update = json_decode(file_get_contents("php://input"), TRUE);
-$temp = file_get_contents('https://api.telegram.org/bot1705693811:AAFRF0B5Z2BNaRC9kIDY1p_gMOI_ZNPz8OM/sendMessage?chat_id=' . $update['chat']['id'] . '&text=' . $update['message']['text']);
+$temp = file_get_contents('https://api.telegram.org/bot1705693811:AAFRF0B5Z2BNaRC9kIDY1p_gMOI_ZNPz8OM/sendMessage?chat_id=' . $update['message']['chat']['id'] . '&text=' . $update['message']['text']);
